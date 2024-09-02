6889e1cd9ffadc2d67343b3dded1bb576a9c58d7
Dysthesis <antheoraviel@protonmail.com>
2024-09-02 18:16:12 +1000

    modules: add wifi module

--- modules/system/core/default.nix
+++ modules/system/core/default.nix
@@ -2,5 +2,6 @@
   imports = [
     ./hardware
     ./boot
+    ./network
   ];
 }
---
+++ modules/system/core/network/wifi/config.nix
@@ -0,0 +1,15 @@
+{
+  config,
+  lib,
+  ...
+}: let
+  cfg = config.laplace.network.wifi.enable;
+  inherit (lib) mkIf;
+in {
+  config = mkIf cfg {
+    networking.networkmanager = {
+      enable = true;
+      wifi.macAddress = "random";
+    };
+  };
+}
