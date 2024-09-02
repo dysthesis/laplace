a84f51bb4719471c6e8acbbfadb9b41d92953355
Dysthesis <antheoraviel@protonmail.com>
2024-09-02 17:52:59 +1000

    modules: add btrfs support

--- modules/system/core/filesystems/btrfs/config.nix
+++ modules/system/core/filesystems/btrfs/config.nix
@@ -1,1 +1,16 @@
-{}
+{
+  lib,
+  config,
+  ...
+}: let
+  inherit (lib) mkIf;
+  inherit (builtins) elem;
+  cfg = config.laplace.filesystems;
+in {
+  config = mkIf (elem "btrfs" cfg) {
+    services.btrfs.autoScrub = {
+      enable = true;
+      interval = "weekly";
+    };
+  };
+}
--- modules/system/core/filesystems/default.nix
+++ modules/system/core/filesystems/default.nix
@@ -1,1 +1,21 @@
-{}
+{
+  lib,
+  config,
+  ...
+}: let
+  inherit (lib.laplace.options) mkEnumOption mkEnumListOption;
+  filesystems = ["btrfs"];
+in {
+  options.laplace.rootFilesystem = mkEnumOption {
+    elems = filesystems;
+    description = "The type of filesystem to use as root";
+  };
+
+  options.laplace.filesystems = mkEnumListOption {
+    elems = filesystems;
+    description = "Which filesystems to enable support for";
+    default = [config.laplace.rootFilesystem];
+  };
+
+  imports = map (filesystem: ./${filesystem}) filesystems;
+}
