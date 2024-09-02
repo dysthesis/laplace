dc4d2244a5dd126a2ed9a1ee9d87c7638f03d259
Dysthesis <antheoraviel@protonmail.com>
2024-09-02 18:29:59 +1000

    modules: add dnscrypt-proxy module

--- modules/system/core/network/default.nix
+++ modules/system/core/network/default.nix
@@ -1,5 +1,6 @@
 {
   imports = [
     ./wifi
+    ./dns
   ];
 }
---
+++ modules/system/core/network/dns/config.nix
@@ -0,0 +1,33 @@
+{
+  config,
+  lib,
+  ...
+}: let
+  inherit (lib) mkIf;
+  cfg = config.laplace.network.dnscrypt-proxy.enable;
+in {
+  config = mkIf cfg {
+    networking = {
+      networkmanager.dns = "none";
+      nameservers = ["127.0.0.1" "::1"];
+      dhcpcd.extraConfig = "nohook resolv.conf";
+    };
+
+    services.dnscrypt-proxy2 = {
+      enable = true;
+      settings = {
+        ipv6_servers = true;
+        require_dnssec = true;
+
+        sources.public-resolvers = {
+          urls = [
+            "https://raw.githubusercontent.com/DNSCrypt/dnscrypt-resolvers/master/v3/public-resolvers.md"
+            "https://download.dnscrypt.info/resolvers-list/v3/public-resolvers.md"
+          ];
+          cache_file = "/var/lib/dnscrypt-proxy2/public-resolvers.md";
+          minisign_key = "RWQf6LRCGA9i53mlYecO4IzT51TGPpvWucNSCh1CBM0QTaLn73Y7GFO3";
+        };
+      };
+    };
+  };
+}
