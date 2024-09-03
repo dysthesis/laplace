c3328b4724322c3a49938ac24e942432b21d3874
Dysthesis <antheoraviel@protonmail.com>
2024-09-04 07:49:56 +1000

    modules: define the configuration for demiurge

--- modules/system/core/users/demiurge/default.nix
+++ modules/system/core/users/demiurge/default.nix
@@ -1,1 +1,52 @@
-{}
+{
+  lib,
+  config,
+  pkgs,
+  ...
+}: let
+  inherit
+    (lib)
+    mkIf
+    ;
+  inherit
+    (builtins)
+    filter
+    hasAttr
+    ;
+
+  cfg = config.laplace.users.demiurge.enable;
+  ifTheyExist = groups:
+    filter
+    (group: hasAttr group config.users.groups)
+    groups;
+in {
+  config = let
+    shell = "zsh";
+  in
+    mkIf cfg {
+      users.users.demiurge = {
+        description = "Demiurge";
+        isNormalUser = true;
+        shell = pkgs.${shell};
+
+        # TODO: Add hashed password with sops-nix
+
+        extraGroups =
+          [
+            "wheel"
+            "video"
+            "audio"
+            "input"
+            "nix"
+            "networkmanager"
+          ]
+          ++ ifTheyExist [
+            "network"
+            "docker"
+            "podman"
+            "libvirt"
+          ];
+      };
+      programs.${shell}.enable = true;
+    };
+}
