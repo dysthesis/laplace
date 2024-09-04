353ba5d48937aea8f44238df0fa7d11f8a443eba
Dysthesis <antheoraviel@protonmail.com>
2024-09-04 18:17:04 +1000

    system: disable default packages

--- modules/system/core/default.nix
+++ modules/system/core/default.nix
@@ -1,4 +1,8 @@
-{
+{lib, ...}: let
+  inherit (lib) mkForce;
+in {
+  # No default packages. From https://xeiaso.net/blog/paranoid-nixos-2021-07-18/
+  environment.defaultPackages = mkForce [];
   imports = [
     ./hardware
     ./boot
