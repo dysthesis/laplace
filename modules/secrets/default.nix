0966fe3a124c98e2a129e28408d8205953e79a7d
Dysthesis <antheoraviel@protonmail.com>
2024-09-04 17:17:51 +1000

    secrets: automatically require hashed password file for enabled users

--- modules/secrets/default.nix
+++ modules/secrets/default.nix
@@ -1,9 +1,35 @@
-{inputs, ...}: {
+{
+  inputs,
+  lib,
+  config,
+  ...
+}: let
+  inherit (builtins) attrNames;
+  inherit
+    (lib)
+    filterAttrs
+    fold
+    ;
+
+  enabledUsers =
+    attrNames
+    (filterAttrs
+      (_name: value: value.enable)
+      config.laplace.users);
+in {
   imports = [inputs.sops-nix.nixosModules.sops];
   sops.defaultSopsFile = ./secrets.yaml;
-  sops.secrets."hashedPasswords/demiurge" = {
-    neededForUsers = true;
-  };
+
+  # Require the hashed passwords for enabled users.
+  sops.secrets =
+    fold
+    (curr: acc:
+      acc
+      // {
+        "hashedPasswords/${curr}".neededForUsers = true;
+      })
+    {}
+    enabledUsers;
 
   # TODO: Adjust this to account for impermanence
   sops.age.keyFile = "/home/demiurge/.config/sops/age/keys.txt";
