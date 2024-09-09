d16b3627255a922c52af46aed0d2daeeae75d297
Dysthesis <antheoraviel@protonmail.com>
2024-09-10 06:44:55 +1000

    Get rid of some programs for sophia

--- modules/home/sophia/yazi/input.nix
+++
@@ -1,7 +0,0 @@
-{
-  programs.yazi.settings.input = {
-    border = {fg = "#89b4fa";};
-    selected = {reversed = true;};
-    title = {};
-  };
-}
--- modules/home/sophia/yazi/tasks.nix
+++
@@ -1,6 +0,0 @@
-{
-  programs.yazi.settings.tasks = {
-    border = {fg = "#89b4fa";};
-    hovered = {underline = true;};
-  };
-}
