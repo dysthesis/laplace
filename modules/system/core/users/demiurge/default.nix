6d4a4833dc35e58a040ff3ebe1994f2cae62453a
Dysthesis <antheoraviel@protonmail.com>
2024-09-12 14:45:35 +1000

    PC ssh key

--- modules/system/core/users/demiurge/default.nix
+++ modules/system/core/users/demiurge/default.nix
@@ -29,7 +29,7 @@ in {
         isNormalUser = true;
         shell = pkgs.${shell};
         openssh.authorizedKeys.keys = [
-          "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJPOoS7+iAo/1p5x2Q+gi5+q5rLoQD7BbIiirRMterti demiurge@adonaios"
+          "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIFN0bzRvuz3JfjOXTtgB5CTC9me7bnCTbudpkBVqkYMN demiurge@adonaios"
           "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOOeZRCxL7/q0UY7ZAAkM5HW6t8RULHu1b7BH3F/n2d2 demiurge@yaldabaoth"
         ];
         hashedPassword = "$y$j9T$WtVEPLB064z6W2eWFUPK81$xT7V9MzUIS.gcoaJzfYjMRY/I5Zi5Hl57XDo9EMwll5";
