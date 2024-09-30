b2bace3d5dce4ff51e959e878744189ce867e460
Dysthesis <antheoraviel@protonmail.com>
2024-10-01 07:21:27 +1000

    Please work

--- modules/system/features/traefik/config.nix
+++ modules/system/features/traefik/config.nix
@@ -9,78 +9,102 @@ in {
   config = mkIf cfg {
     sops.secrets.traefik = {};
     systemd.services.traefik.serviceConfig.EnvironmentFile = [config.sops.secrets.traefik.path];
-    services.traefik = {
-      enable = true;
-
-      staticConfigOptions = {
-        global = {
-          checkNewVersion = false;
-          sendAnonymousUsage = false;
-        };
-        log.level = "DEBUG";
-
-        api.dashboard = true;
-        certificatesResolvers = {
-          default.acme = {
-            email = "acme.dictate699@simplelogin.com";
-            storage = "/var/lib/traefik/acme.json";
-            # caServer = "https://acme-v02.api.letsencrypt.org/directory";
-            dnsChallenge = {
-              provider = "cloudflare";
-              resolvers = ["1.1.1.1:53" "8.8.8.8:53"];
-            };
-          };
-        };
-
-        entryPoints = {
-          web = {
-            address = ":80";
-            http = {
-              redirections = {
-                entryPoint = {
-                  to = "websecure";
-                  scheme = "https";
-                };
-              };
-            };
-          };
-          websecure = {
-            address = ":443";
-            http = {
-              tls = {
-                options = "default";
-              };
-            };
-          };
-        };
+    security.acme = {
+      acceptTerms = true;
+      defaults.email = "acme.dictate699@simplelogin.com";
+      certs."dysthesis.com" = {
+        group = "traefik";
+        domain = "dysthesis.com";
+        extraDomainNames = ["*.domain.com"];
+        dnsProvider = "cloudflare";
+        dnsPropagationCheck = true;
+        credentialsFile = config.sops.secrets.traefik.path;
       };
-      dynamicConfigOptions =
-        {
-          http = {
-            routers =
-              {
-                api = {
-                  entrypoints = ["traefik"];
-                  rule = "PathPrefix(`/api/`)";
-                  service = "api@internal";
-                };
-              }
-              // mkIf config.laplace.features.miniflux.enable {
-                miniflux = {
-                  rule = "Host(`rss.dysthesis.com`)";
-                  entrypoints = ["websecure"];
-                  service = "miniflux";
-                  tls = {
-                    domains = [{main = "*.dysthesis.com";}];
-                    certresolver = "default";
+    };
+    services.traefik =
+      {
+        enable = true;
+
+        staticConfigOptions = {
+          global = {
+            checkNewVersion = false;
+            sendAnonymousUsage = false;
+          };
+          log.level = "DEBUG";
+
+          api.dashboard = true;
+          certificatesResolvers = {
+            default.acme = {
+              email = "acme.dictate699@simplelogin.com";
+              storage = "/var/lib/traefik/acme.json";
+              caServer = "https://acme-v02.api.letsencrypt.org/directory";
+              dnsChallenge = {
+                provider = "cloudflare";
+                resolvers = ["1.1.1.1:53" "8.8.8.8:53"];
+              };
+            };
+          };
+
+          entryPoints = {
+            web = {
+              address = ":80";
+              http = {
+                redirections = {
+                  entryPoint = {
+                    to = "websecure";
+                    scheme = "https";
                   };
                 };
               };
+            };
+            websecure = {
+              address = ":443";
+              http = {
+                tls = {
+                  options = "default";
+                };
+                stores.default = {
+                  defaultCertificate = {
+                    certFile = "/var/lib/acme/dysthesis.com/cert.pem";
+                    keyFile = "/var/lib/acme/dysthesis.com/key.pem";
+                  };
+                };
+
+                certificates = [
+                  {
+                    certFile = "/var/lib/acme/dysthesis.com/cert.pem";
+                    keyFile = "/var/lib/acme/dysthesis.com/key.pem";
+                    stores = "default";
+                  }
+                ];
+              };
+            };
           };
-        }
-        // mkIf config.laplace.features.miniflux.enable {
-          services.miniflux.loadBalancer.servers = [{url = "http://${config.services.miniflux.config.LISTEN_ADDR}";}];
         };
-    };
+        dynamicConfigOptions.http = {
+          routers =
+            {
+              api = {
+                entrypoints = ["traefik"];
+                rule = "PathPrefix(`/api/`)";
+                service = "api@internal";
+              };
+            }
+            // mkIf config.laplace.features.miniflux.enable {
+              miniflux = {
+                rule = "Host(`rss.dysthesis.com`)";
+                entrypoints = ["websecure"];
+                service = "miniflux";
+                tls = {
+                  domains = [{main = "*.dysthesis.com";}];
+                  certresolver = "default";
+                };
+              };
+            };
+        };
+      }
+      // mkIf config.laplace.features.miniflux.enable {
+        services.miniflux.loadBalancer.servers = [{url = "http://${config.services.miniflux.config.LISTEN_ADDR}";}];
+      };
   };
 }
