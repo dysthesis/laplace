c200cf80e74284e4733b4e6363d618c753c3f0b6
Dysthesis <antheoraviel@protonmail.com>
2024-10-02 12:12:18 +1000

    PLEASE

--- modules/system/features/traefik/config.nix
+++ modules/system/features/traefik/config.nix
@@ -81,7 +81,7 @@ in {
         };
         services = {
           miniflux.loadBalancer.servers = [{url = "http://${config.services.miniflux.config.LISTEN_ADDR}";}];
-          forgejo.loadBalancer.servers = [{url = "http://localhost:${toString config.services.forgejo.settings.server.HTTP_PORT}";}];
+          forgejo.loadBalancer.servers = [{url = "http://0.0.0.0:${toString config.services.forgejo.settings.server.HTTP_PORT}";}];
           gitssh.loadBalancer.servers = [{address = "localhost:2222";}];
         };
       };
