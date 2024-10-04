891befc174b4a86b8d4aee9e38532258fe351de6
Dysthesis <antheoraviel@protonmail.com>
2024-10-04 18:27:13 +1000

    Try to fix owntracks complaining about permissions

--- modules/system/features/services/owntracks/config.nix
+++ modules/system/features/services/owntracks/config.nix
@@ -8,20 +8,28 @@
   cfg = config.laplace.features.services.owntracks;
 in {
   config = mkIf cfg.enable {
-    systemd.services.owntracks = {
-      enable = true;
+    systemd = {
+      services.owntracks = {
+        enable = true;
 
-      description = "Owntracks";
-      wantedBy = ["multi-user.target"];
+        description = "Owntracks";
+        wantedBy = ["multi-user.target"];
 
-      serviceConfig = {
-        ExecStart = "${pkgs.owntracks-recorder}/bin/ot-recorder -S /var/lib/owntracks --doc-root ${pkgs.owntracks-recorder}/docroot --port 0 --http-host 127.0.0.1 --http-port ${toString cfg.port}";
-        StateDirectory = "owntracks";
-        DynamicUser = true;
+        serviceConfig = {
+          ExecStart = "${pkgs.owntracks-recorder}/bin/ot-recorder -S /var/lib/owntracks --doc-root ${pkgs.owntracks-recorder}/docroot --port 0 --http-host 127.0.0.1 --http-port ${toString cfg.port}";
+          StateDirectory = "owntracks";
+          DynamicUser = true;
 
-        Restart = "on-failure";
-        KillSignal = "SIGINT";
+          Restart = "on-failure";
+          KillSignal = "SIGINT";
+        };
       };
+
+      tmpfiles.rules = [
+        "d /var/lib/private 0700 root root -"
+        "d /var/lib/private/miniflux 0750 miniflux miniflux -"
+        "d /var/lib/private/dnscrypt-proxy 0755 dnscrypt-proxy2 dnscrypt-proxy2 -"
+      ];
     };
   };
 }
