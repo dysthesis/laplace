d128f7753855adae86cedfd3a46857b7809aee47
Dysthesis <antheoraviel@protonmail.com>
2024-11-10 12:07:18 +1100

    New ssh key

--- modules/system/core/users/demiurge/default.nix
+++ modules/system/core/users/demiurge/default.nix
@@ -31,6 +31,7 @@ in {
         openssh.authorizedKeys.keys = [
           "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIPxBpd1Xyr16hfHFvd/AvOz8mLehUCI28QW3WNht4Xkn demiurge@adonaios"
           "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOOeZRCxL7/q0UY7ZAAkM5HW6t8RULHu1b7BH3F/n2d2 demiurge@yaldabaoth"
+          "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJazHjq2dgnKO3SmC2Z6mfoVk4cmdk0digem0bLZSYHF demiurge@erebus"
         ];
         hashedPassword = "$y$j9T$WtVEPLB064z6W2eWFUPK81$xT7V9MzUIS.gcoaJzfYjMRY/I5Zi5Hl57XDo9EMwll5";
         extraGroups =
