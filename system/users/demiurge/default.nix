1a1ff38e0a0514cedeacafef4afe3a4fa66d9e8a
Dysthesis <antheoraviel@protonmail.com>
2025-02-13 16:58:38 +1100

    Formatting

--- system/users/demiurge/default.nix
+++ system/users/demiurge/default.nix
@@ -5,19 +5,17 @@
   config,
   pkgs,
   ...
-}: let
-  inherit
-    (pkgs)
+}:
+let
+  inherit (pkgs)
     writeText
     system
     ;
 
-  inherit
-    (lib)
+  inherit (lib)
     mkIf
     ;
-  inherit
-    (builtins)
+  inherit (builtins)
     elem
     filter
     hasAttr
@@ -26,24 +24,25 @@
   wm = inputs.gungnir.packages.${system}.dwm;
   wallpaper = ./${self}/assets/wallpaper.png;
 
-  xinitrc = with pkgs;
+  xinitrc =
+    with pkgs;
     writeText ".xinitrc"
-    # sh
-    ''
-      # turn off Display Power Management Service (DPMS)
-      xset -dpms
-      setterm -blank 0 -powerdown 0
+      # sh
+      ''
+        # turn off Display Power Management Service (DPMS)
+        xset -dpms
+        setterm -blank 0 -powerdown 0
 
-      # turn off black Screensaver
-      xset s off
+        # turn off black Screensaver
+        xset s off
 
-      # Start some services
-      ${dunst} &
-      ${udiskie} &
-      ${hsetroot} ${wallpaper} &
-      ${inputs.gungnir.packages.${system}.dwm-bar} &
-      exec ${wm}
-    '';
+        # Start some services
+        ${dunst} &
+        ${udiskie} &
+        ${hsetroot} ${wallpaper} &
+        ${inputs.gungnir.packages.${system}.dwm-bar} &
+        exec ${wm}
+      '';
 
   xinit-dwm = pkgs.stdenv.mkDerivation rec {
     name = "xinit-dwm";
@@ -52,9 +51,7 @@
       xorg.xinit
     ];
     installPhase =
-      /*
-      sh
-      */
+      # sh
       ''
         makeWrapper ${lib.getExe pkgs.xorg.xinit} $out/bin/${name} \
         	--add-flags ${xinitrc}
@@ -63,13 +60,14 @@
 
   cfg = elem "demiurge" config.laplace.users;
   ifTheyExist = groups: filter (group: hasAttr group config.users.groups) groups;
-in {
+in
+{
   config = mkIf cfg {
     users.users.demiurge = {
       description = "Demiurge";
       shell = pkgs.bash;
       isNormalUser = true;
-      openssh.authorizedKeys.keys = [];
+      openssh.authorizedKeys.keys = [ ];
       hashedPassword = "$y$j9T$WtVEPLB064z6W2eWFUPK81$xT7V9MzUIS.gcoaJzfYjMRY/I5Zi5Hl57XDo9EMwll5";
       extraGroups =
         [
@@ -86,7 +84,8 @@ in {
           "podman"
           "libvirt"
         ];
-      packages = with pkgs;
+      packages =
+        with pkgs;
         [
           signal-desktop
         ]
