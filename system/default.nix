989324c53354e9e8899714ecb20e99d8d2443e37
Dysthesis <antheoraviel@protonmail.com>
2025-02-15 09:34:42 +1100

    See if this fixes NetworkManager

--- system/default.nix
+++ system/default.nix
@@ -1,8 +1,26 @@
-{ lib, ... }:
-let
-  inherit (lib.babel.modules) importInDirectory;
-in
 {
-  config.system.stateVersion = "24.11";
+  pkgs,
+  lib,
+  ...
+}: let
+  inherit (lib.babel.modules) importInDirectory;
+in {
+  config = {
+    services = {
+      gnome.gnome-keyring.enable = true;
+      dbus = {
+        packages = with pkgs; [
+          dconf
+          gcr
+          udisks2
+        ];
+        implementation = "broker";
+        enable = true;
+      };
+    };
+    security.pam.services.login.enableGnomeKeyring = true;
+
+    system.stateVersion = "24.11";
+  };
   imports = importInDirectory ./.;
 }
