ec709fe3b55516d18cab7cb0d34f5b4e16b4cd6e
Dysthesis <antheoraviel@protonmail.com>
2025-02-24 09:52:02 +1100

    Fix disk layout for deimos

--- hosts/phobos/disks.nix
+++ hosts/phobos/disks.nix
@@ -1,7 +1,6 @@
 let
   inherit (builtins) mapAttrs;
-in
-{
+in {
   disko.devices = {
     disk.main = {
       device = "/dev/nvme0n1";
@@ -17,7 +16,7 @@ in
               type = "filesystem";
               format = "vfat";
               mountpoint = "/boot";
-              mountOptions = [ "defaults" ];
+              mountOptions = ["defaults"];
             };
           };
 
@@ -32,29 +31,25 @@ in
 
               content = {
                 type = "btrfs";
-                extraArgs = [ "-f" ];
+                extraArgs = ["-f"];
 
-                subvolumes =
-                  let
-                    mountOptions = [
-                      "defaults"
-                      "ssd"
-                      "noatime"
-                      "compress=zstd"
-                      "space_cache=v2"
-                      "discard=async"
-                      "autodefrag"
-                    ];
-                  in
-                  mapAttrs (_name: value: value // { mountOptions = mountOptions ++ [ "noexec" ]; }) {
+                subvolumes = let
+                  mountOptions = [
+                    "defaults"
+                    "ssd"
+                    "noatime"
+                    "compress=zstd"
+                    "space_cache=v2"
+                    "discard=async"
+                    "autodefrag"
+                  ];
+                in
+                  mapAttrs (_name: value: value // {inherit mountOptions;}) {
+                    "@nix".mountpoint = "/nix";
                     "@persist".mountpoint = "/nix/persist";
                     "@home".mountpoint = "/home";
                   }
                   // {
-                    "@nix" = {
-                      mountpoint = "/nix";
-                      inherit mountOptions;
-                    };
                     "@swap" = {
                       mountpoint = "/swap";
                       swap.swapfile.size = "8G";
@@ -67,21 +62,16 @@ in
       };
     };
 
-    # Make root and home impermanent
-    nodev =
-      let
+    # Make root impermanent
+    nodev = {
+      "/" = {
+        fsType = "tmpfs";
         mountOptions = [
           "size=2G"
           "defaults"
           "mode=755"
-          "noexec"
         ];
-      in
-      {
-        "/" = {
-          fsType = "tmpfs";
-          inherit mountOptions;
-        };
       };
+    };
   };
 }
