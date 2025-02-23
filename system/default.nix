d09d5677de5f8d4d6f6e8e964c591b28b59fba57
Dysthesis <antheoraviel@protonmail.com>
2025-02-24 09:30:36 +1100

    Get rid of sshd

--- system/default.nix
+++ system/default.nix
@@ -2,26 +2,10 @@
   pkgs,
   lib,
   ...
-}:
-let
+}: let
   inherit (lib.babel.modules) importInDirectory;
-in
-{
+in {
   config = {
-    # TODO: Move this
-    services.openssh = {
-      enable = lib.mkDefault true;
-      settings = {
-        PermitRootLogin = lib.mkForce "no";
-        UseDns = false;
-        X11Forwarding = false;
-        PasswordAuthentication = lib.mkForce false;
-        KbdInteractiveAuthentication = false;
-      };
-      openFirewall = true;
-      ports = [ 22 ];
-    };
-
     console = {
       earlySetup = true;
       font = "${pkgs.terminus_font}/share/consolefonts/ter-120n.psf.gz";
