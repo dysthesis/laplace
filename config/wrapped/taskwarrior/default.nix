84faf10026287d8b41734d6c594e0285cfbc512f
Dysthesis <antheoraviel@protonmail.com>
2025-02-25 18:03:24 +1100

    Configure urgency coefficients for projects

--- config/wrapped/taskwarrior/default.nix
+++ config/wrapped/taskwarrior/default.nix
@@ -2,18 +2,29 @@
   lib,
   pkgs,
   ...
-}:
-let
+}: let
   inherit (lib.babel.pkgs) mkWrapper;
   inherit (pkgs) writeText;
+  inherit (lib.attrsets) mapAttrsToList;
+  inherit (builtins) concatStringsSep;
+
+  projectUrgency = {
+    COMP3121 = 3;
+    COMP3900 = 2;
+    COMP4920 = 2;
+    University = 2;
+  };
+  mkProjectUrgency = mapAttrsToList (proj: urgency: "urgency.user.project.${proj}.coefficient=${toString urgency}");
+
   taskrc = writeText ".taskrc" ''
+    ${concatStringsSep "\n" (mkProjectUrgency projectUrgency)}
     verbose=blank,footnote,label,new-id,affected,edit,special,project,sync,unwait
-     weekstart=monday
-     news.version=2.6.0
-     data.location=~/.local/share/task
+    weekstart=monday
+    news.version=2.6.0
+    data.location=~/.local/share/task
   '';
 in
-mkWrapper pkgs pkgs.taskwarrior ''
-  wrapProgram $out/bin/task \
-   --set TASKRC ${taskrc} \
-''
+  mkWrapper pkgs pkgs.taskwarrior ''
+    wrapProgram $out/bin/task \
+     --set TASKRC ${taskrc} \
+  ''
