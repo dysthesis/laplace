d1511a46650cd86e9248eaa6cf278de20f897cb0
Dysthesis <antheoraviel@protonmail.com>
2025-03-21 11:44:58 +1100

    Get rid of emacs

--- system/users/demiurge/default.nix
+++ system/users/demiurge/default.nix
@@ -4,16 +4,18 @@
   config,
   pkgs,
   ...
-}:
-let
-  inherit (pkgs)
+}: let
+  inherit
+    (pkgs)
     system
     ;
 
-  inherit (lib)
+  inherit
+    (lib)
     mkIf
     ;
-  inherit (builtins)
+  inherit
+    (builtins)
     elem
     filter
     hasAttr
@@ -21,8 +23,7 @@ let
 
   cfg = elem "demiurge" config.laplace.users;
   ifTheyExist = groups: filter (group: hasAttr group config.users.groups) groups;
-in
-{
+in {
   config = mkIf cfg {
     users.users.demiurge = {
       description = "Demiurge";
@@ -47,8 +48,7 @@ in
           "podman"
           "libvirt"
         ];
-      packages =
-        with pkgs;
+      packages = with pkgs;
         [
           signal-desktop
           btop
@@ -65,7 +65,7 @@ in
           inputs.mandelbrot.packages.${pkgs.system}.xmobar
           protonvpn-gui
           networkmanagerapplet
-          (pkgs.uutils-coreutils.override { prefix = ""; })
+          (pkgs.uutils-coreutils.override {prefix = "";})
         ]
         ++ (with pkgs.configured; [
           fish
@@ -90,19 +90,19 @@ in
         ++ [
           inputs.poincare.packages.${system}.default
           inputs.daedalus.packages.${system}.default
-          inputs.jormungandr.packages.${system}.default
         ]
         ++ (
-          with inputs.gungnir.packages.${system};
-          let
-            fontSize = if config.networking.hostName == "phobos" then 15 else 12;
-          in
-          [
+          with inputs.gungnir.packages.${system}; let
+            fontSize =
+              if config.networking.hostName == "phobos"
+              then 15
+              else 12;
+          in [
             (st {
               inherit fontSize;
               borderpx = 20;
             })
-            (dmenu { inherit fontSize; })
+            (dmenu {inherit fontSize;})
           ]
         );
     };
