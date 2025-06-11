f8d9679ce13c29215b66b8ab9f18739df2c5c603
Dysthesis <antheoraviel@protonmail.com>
2025-06-11 13:52:43 +1000

    More jujutsu config

--- config/wrapped/jujutsu/config.nix
+++ config/wrapped/jujutsu/config.nix
@@ -2,27 +2,30 @@
   pkgs,
   lib,
   ...
-}:
-let
+}: let
   inherit (lib) getExe;
 in
-with pkgs;
-{
-  user = {
-    name = "Dysthesis";
-    email = "antheoraviel@protonmail.com";
-  };
-  signing = {
-    behaviour = "own";
-    backend = "gpg";
-    key = "4F41D2DFD42D5568";
-  };
-  ui = {
-    diff.tool = [
-      "${getExe difftastic}"
-      "--color=always"
-      "$left"
-      "$right"
-    ];
-  };
-}
+  with pkgs; {
+    user = {
+      name = "Dysthesis";
+      email = "antheoraviel@protonmail.com";
+    };
+    template-aliases."format_timestamp(timestamp)" = "timestamp.ago()";
+    git.sign-on-push = "true";
+    signing = {
+      behaviour = "own";
+      backend = "gpg";
+      key = "4F41D2DFD42D5568";
+    };
+    aliases = {
+      tug = ["bookmark" "move" "--from" "heads(::@- & bookmarks())" "--to" "@-"];
+    };
+    ui = {
+      diff.tool = [
+        "${getExe difftastic}"
+        "--color=always"
+        "$left"
+        "$right"
+      ];
+    };
+  }
