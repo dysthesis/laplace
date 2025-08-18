910818551e59a2282d37c75f641842800e5babc0
Dysthesis <antheoraviel@protonmail.com>
2025-08-18 15:38:07 +1000

    Changes

--- system/profiles/desktop/default.nix
+++ system/profiles/desktop/default.nix
@@ -8,6 +8,47 @@
   inherit (lib) mkIf;
   inherit (builtins) elem;
   cfg = config.laplace.profiles;
+
+  browser = ["zen"];
+
+  xdgAssociations = type: program: list:
+    builtins.listToAttrs (map (e: {
+        name = "${type}/${e}";
+        value = program;
+      })
+      list);
+
+  browserTypes =
+    (xdgAssociations "application" browser [
+      "json"
+      "x-extension-htm"
+      "x-extension-html"
+      "x-extension-shtml"
+      "x-extension-xht"
+      "x-extension-xhtml"
+      "xhtml+xml"
+    ])
+    // (xdgAssociations "x-scheme-handler" browser [
+      "about"
+      "chrome"
+      "ftp"
+      "http"
+      "https"
+      "unknown"
+    ]);
+
+  # XDG MIME types
+  associations = builtins.mapAttrs (_: v: (map (e: "${e}.desktop") v)) ({
+      "application/pdf" = ["org.pwmt.zathura-pdf-mupdf"];
+      "text/html" = browser;
+      "text/plain" = ["Helix"];
+      "inode/directory" = ["yazi"];
+      "x-scheme-handler/magnet" = ["transmission-gtk"];
+      # Full entry is org.telegram.desktop.desktop
+      "x-scheme-handler/tg" = ["org.telegram.desktop"];
+      "x-scheme-handler/tonsite" = ["org.telegram.desktop"];
+    }
+    // browserTypes);
 in {
   config = mkIf (elem "desktop" cfg) {
     systemd = {
@@ -114,7 +155,10 @@ in {
     xdg = {
       autostart.enable = true;
       icons.enable = true;
-      mime.enable = true;
+      mime = {
+        enable = true;
+        defaultApplications = associations;
+      };
       sounds.enable = true;
       portal = {
         enable = true;
