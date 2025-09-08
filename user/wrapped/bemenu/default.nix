1c2aa2648f8f8c9424cc8272d4a5cf8acaec3146
Dysthesis <antheoraviel@protonmail.com>
2025-09-08 11:52:03 +1000

    Adaptive bemenu size

--- user/wrapped/bemenu/default.nix
+++ user/wrapped/bemenu/default.nix
@@ -16,29 +16,51 @@ let
   });
   inherit (lib.babel.pkgs) mkWrapper;
   inherit (builtins) concatStringsSep;
-  flags =
-    let
-      cfg = config.networking.hostName == "phobos";
-      fontSize = if cfg then 9 else 9;
-    in
-    [
-      "-b" # bottom
-      ''-z'' # fuzzy
-      ''-i'' # ignorecase
-      ''-p \" \"'' # prompt
-      ''--fn \"JBMono Nerd Font ${toString fontSize}\"''
-      ''-H \"24\"''
-      ''--hp \"8\"''
-      ''--fb \"#000000\"''
-      ''--ff \"#ffffff\"''
-      ''--nb \"#000000\"''
-      ''--nf \"#ffffff\"''
-      ''--tb \"#7788AA\"''
-      ''--hb \"#080808\"''
-      ''--tf \"#000000\"''
-      ''--hf \"#7788AA\"''
-      ''--ab \"#000000\"''
-    ];
+  # Adaptive sizing based on primary monitor, similar to waybar/style.nix
+  monitors = config.laplace.hardware.monitors or [];
+  defaultMonitor =
+    if monitors == [] then
+      {
+        width = 1920;
+        height = 1080;
+      }
+    else
+      builtins.head monitors;
+  primaryMonitor = lib.findFirst (m: (m.primary or false)) defaultMonitor monitors;
+
+  clamp = min: max: val: lib.max min (lib.min max val);
+  round = x: builtins.floor (x + 0.5);
+
+  baselineH = 1080.0;
+  rawScale = baselineH / (primaryMonitor.height or baselineH);
+  scale = clamp 0.6 1.2 rawScale;
+
+  baseFont = 9.0;
+  baseLineH = 24.0;
+  baseHP = 8.0;
+
+  fontSize = round (baseFont * scale);
+  lineHeight = round (baseLineH * scale);
+  hp = lib.max 1 (round (baseHP * scale));
+
+  flags = [
+    "-b" # bottom
+    ''-z'' # fuzzy
+    ''-i'' # ignorecase
+    ''-p \" \"'' # prompt
+    ''--fn \"JBMono Nerd Font ${toString fontSize}\"''
+    ''-H \"${toString lineHeight}\"''
+    ''--hp \"${toString hp}\"''
+    ''--fb \"#000000\"''
+    ''--ff \"#ffffff\"''
+    ''--nb \"#000000\"''
+    ''--nf \"#ffffff\"''
+    ''--tb \"#7788AA\"''
+    ''--hb \"#080808\"''
+    ''--tf \"#000000\"''
+    ''--hf \"#7788AA\"''
+    ''--ab \"#000000\"''
+  ];
   flags' = flags |> map (flag: ''--add-flags "${flag}"'') |> concatStringsSep " ";
 in
 mkWrapper pkgs bemenu
